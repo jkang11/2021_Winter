461ff7ed3a53d76d601fa7d2f71179bedc27268a
jkang11 <72840822+jkang11@users.noreply.github.com>
2021-02-28 14:21:53 -0800

    Update javascript.js

--- lab3c/javascript.js
+++ lab3c/javascript.js
@@ -2,7 +2,7 @@ var map = L.map('map').setView([47.2529, -122.4443], 11.5);
 L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
     attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
     maxZoom: 18,
-    id: 'mapbox/streets-v11',
+    id: 'mapbox/outdoors-v9',
     tileSize: 512,
     zoomOffset: -1,
     accessToken: 'pk.eyJ1IjoiamthbmcxIiwiYSI6ImNrZ3d5NjR3NDBldzgycW8zdDZ0NTZvYjAifQ.lUQQ9nNNJryYS05ye4FpLA'
